9bc11471d0b5fe74e4dcdc7139d9645d848db37e
Magdalena Rogalska <m.rogalska1113@gmail.com>
2024-09-30 14:20:16 -0400

    testing writing times

--- Assets/Scripts/C2M2/Utils/CSVWriter.cs
+++ Assets/Scripts/C2M2/Utils/CSVWriter.cs
@@ -23,28 +23,24 @@ namespace C2M2.Utils
         private GameManager gm = null;
         private float sTime;
         public bool single = false;
+        private int numRows=0;
+        private Stopwatch stopwatch;
+        private long elapsed;
+        private int times = 1;
+        private float wtime;
+        private float utime;
         void Awake()
         {   
             gm = GameManager.instance;
             sim = (SparseSolverTestv1)gm.activeSims[0];
-            if (single)
-            {
-                //get graph
-                UnityEngine.Debug.Log("Got the graph");
-                graph = GetComponent<NDLineGraph>();
-
-
-            }
             
-            
-
         }
         
         //Start is called on the frame when a script is enabled just before any of the Update methods are called the first time. This function can be a coroutine.
         private void Start()
-        {   gm = GameManager.instance;
-            sim = (SparseSolverTestv1)gm.activeSims[0];
+        {   
             int size = sim.Neuron.nodes.Count;
+            
             DateTime date = DateTime.Now;
             String formatted = date.ToString("MM-dd-yyyy-hh-mm-ss");
             String fname = "/neuro_visor_recording_"+formatted+".csv";
@@ -55,20 +51,19 @@ namespace C2M2.Utils
             TextWriter tw = new StreamWriter(filename, false);
             
             tw.Write("Time (ms)");
-            if (single == false)
-            {
-                for (int i = 0; i < size; i++)
-                {
-                    tw.Write(", Vert["+i+"]");
-                }
+            
+            for (int i = 0; i < size*times; i++)
+            { tw.Write(", Vert["+i+"]");
             }
-            else
-            {
-                tw.Write(", Vert["+graph.ndgraph.FocusVert+"]");
-            }
+            tw.Write(", Update ms");
+            tw.Write(", Write ms");
+            
             
             tw.Close();
+            stopwatch = new Stopwatch();
             
+            
+
 
 
 
@@ -76,10 +71,11 @@ namespace C2M2.Utils
 
         private void Update()
         {
-            
+            stopwatch.Restart();
             cellData = sim.Get1DValues();
             sTime = sim.GetSimulationTime()*1000;
-            
+            stopwatch.Stop();
+            elapsed = stopwatch.ElapsedMilliseconds;
             WriteToCSV();
             
             
@@ -92,37 +88,51 @@ namespace C2M2.Utils
         public void WriteToCSV()
         {
             Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
+            long elapsedMilliseconds = 0;
+            stopWatch.Restart();
             
             
-            if (single)
+            
+            if (cellData.Length > 0 && numRows<1000)
             {
-                TextWriter tw = new StreamWriter(filename, true);
-                tw.Write("\n"+sTime);
-                tw.Write(","+cellData[graph.ndgraph.FocusVert]*sim.unitScaler);
-            }
-            else if (cellData.Length > 0)
-            {
+                numRows++;
                 
                 TextWriter tw = new StreamWriter(filename, true);
                 tw.Write("\n"+sTime);
-                for (int i = 0; i < cellData.Length; i++)
+                for (int j = 1; j <= times;j++)
                 {
-                    tw.Write(","+cellData[i]*sim.unitScaler);
+
+
+                    for (int i = 0; i < cellData.Length; i++)
+                    {
+                        tw.Write("," + cellData[i] * sim.unitScaler);
+                    }
                 }
-            
+
+
+                stopWatch.Stop();
+                tw.Write((","+elapsed));
+                utime += elapsed;
+                elapsedMilliseconds = stopWatch.ElapsedMilliseconds;
+                tw.Write(","+elapsedMilliseconds);
+                wtime += elapsedMilliseconds;
                 tw.Close();
+            }
+            else
+            {
+                UnityEngine.Debug.Log("Update avg: " + (utime/1000));
+                UnityEngine.Debug.Log("Write avg: " + (wtime/1000));
             }
             
             
-            stopWatch.Stop();
-            
-            long elapsedMilliseconds = stopWatch.ElapsedMilliseconds;
-            
-            string elapsedTime = $"{elapsedMilliseconds} ms";
             
             
-            UnityEngine.Debug.Log("Writing to CSV RunTime: " + elapsedTime);
+            
+            
+            // string elapsedTime = $"{elapsedMilliseconds} ms";
+            //
+            //
+            // UnityEngine.Debug.Log("Writing to CSV RunTime: " + elapsedTime);
             
             
         }
